e7c70faf2af47d9172a986082f24ca0b23f850ba
joshua360x <jkw5499@psu.edu>
2022-03-21 16:02:43 -0400

    added test 4 successfully

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -9,4 +9,4 @@ LEFT JOIN film_category
 ON film.film_id = film_category.film_id
 LEFT JOIN category
 ON film_category.category_id = category.category_id
-WHERE category.name = 'Action'
+WHERE category.name = 'Action';
