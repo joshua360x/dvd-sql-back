e94f91788e8fe1bc39fb353640665589a7a73f60
joshua360x <jkw5499@psu.edu>
2022-03-21 15:17:58 -0400

    added test 2 successfully

--- exercise_2/exercise-2.test.js
+++ exercise_2/exercise-2.test.js
@@ -1,13 +1,13 @@
-// const fs = require('fs');
-// const pool = require('../lib/utils/pool');
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
 
-// describe('exercise 2', () => {
-//   afterAll(() => pool.end());
+describe('exercise 2', () => {
+  afterAll(() => pool.end());
 
-//   it('find all rental dates for customers with the first_name Patricia', async () => {
-//     const { rows } = await pool.query(
-//       fs.readFileSync(`${__dirname}/exercise-2.sql`, 'utf-8')
-//     );
-//     expect(rows.length).toEqual(27);
-//   });
-// });
+  it('find all rental dates for customers with the first_name Patricia', async () => {
+    const { rows } = await pool.query(
+      fs.readFileSync(`${__dirname}/exercise-2.sql`, 'utf-8')
+    );
+    expect(rows.length).toEqual(27);
+  });
+});
